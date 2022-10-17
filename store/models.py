868ff6cebf44d5b7d6111412cc1df26135815159
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-10-17 22:12:58 +0300

    create models for store and tag apps

--- store/models.py
+++ store/models.py
@@ -1,19 +1,86 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    """ Create Collection model """
+    title = models.CharField(max_length=256)
+
+
+class Promotion(models.Model):
+    """ Create Promotion model and associate many-to-many relation with product model """
+    description = models.CharField(max_length=256)
+    discount = models.FloatField()
+    # solve circular dependency using plus sign to avoid creating the reverse relationship
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
 class Product(models.Model):
-    """ Create product model """
+    """ Create Product model and associate many-to-one relation with collection model """
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    # Set on-delete to protect in order to prevent deleting all the products in the collection
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    # use plural "promotions" to indicate many-to-many- relation with promotion model
+    promotions = models.ManyToManyField(Promotion, related_name="products")
 
 
 class Customer(models.Model):
-    """ Create customer model """
+    """ Create Customer model """
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SLIVER = "S"
+    MEMBERSHIP_GOLD = "G"
+    # Set choices for membership and use bronze membership as default
+    MEMBERSHIP_CHOICES = [(MEMBERSHIP_BRONZE, "Bronze"), (MEMBERSHIP_SLIVER, "Sliver"), (MEMBERSHIP_GOLD, "Gold")]
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=32)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    """ Create Order model and associate many-to-one relation with customer model """
+    PAYMENT_STATUS_PENDING = "P"
+    PAYMENT_STATUS_COMPLETE = "C"
+    PAYMENT_STATUS_FAILED = "F"
+    # Set choices for payment status and use pending status as default
+    PAYMENT_STATUS_CHOICES = [(PAYMENT_STATUS_PENDING, "Pending"), (PAYMENT_STATUS_COMPLETE, "Complete"),
+                              (PAYMENT_STATUS_FAILED, "Failed")]
+    created_at = models.DateTimeField(auto_now=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    # Set on-delete to protect in order to prevent deleting all the orders when customer is deleted
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class OrderItem(models.Model):
+    """ Create OrderItem model and associate many-to-one relation with order and product models """
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    # Set unit_price to calculate the price at the time of order to allow varied prices
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    """ Create Cart model """
+    # auto_now_add populates only on creation
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    """ Create CartItem model and associate many-to-one relation with cart and product models """
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+
+class Address(models.Model):
+    """ Create Address model and associate many-to-one relation with customer model """
+    street = models.CharField(max_length=256)
+    city = models.CharField(max_length=256)
+    # Set one-to-many relationship with customer where customer can have multiple addresses
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    """ Create Tag model """
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    """ Create TagItem model which applies generic tag using content_type """
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
