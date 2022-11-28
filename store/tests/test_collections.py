2ddd4217ee182cb034a6477b1c90302460e3e055
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-11-28 23:56:12 +0300

    implement the rest test cases for creation tests for collections endpoint

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,4 +1,5 @@
 """ Test for collections endpoint where directory, file and method names are conventions """
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
@@ -7,6 +8,7 @@ import pytest
 @pytest.mark.django_db
 class TestCreateCollection():
     """ Test cases for creating a collection for collections endpoint """
+
     @pytest.mark.skip
     def test_if_user_is_anonymous_returns_401(self):
         # Test workflow consists of three parts arrange, act and assert actions
@@ -18,3 +20,41 @@ class TestCreateCollection():
         response = api_client.post("/store/collections/", {"title": "new collection"})
         # Assert:
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        # Arrange:
+        # Nothing to arrange since this test is for creation
+        # Act:
+        api_client = APIClient()
+        # Test using authenticated user
+        api_client.force_authenticate(user={})
+        # Ensure that the url starts with / in the http method
+        response = api_client.post("/store/collections/", {"title": "new collection"})
+        # Assert:
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self):
+        # Arrange:
+        # Nothing to arrange since this test is for creation
+        # Act:
+        api_client = APIClient()
+        # Test using authenticated admin user
+        api_client.force_authenticate(user=User(is_staff=True))
+        # Set invalid title to test invalid data test case
+        response = api_client.post("/store/collections/", {"title": ""})
+        # Assert:
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data["title"] is not None
+
+    def test_if_data_is_valid_returns_201(self):
+        # Arrange:
+        # Nothing to arrange since this test is for creation
+        # Act:
+        api_client = APIClient()
+        # Test using authenticated admin user
+        api_client.force_authenticate(user=User(is_staff=True))
+        # Set invalid title to test invalid data test case
+        response = api_client.post("/store/collections/", {"title": "new collection"})
+        # Assert:
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["id"] > 0
