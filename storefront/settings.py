d86ce3dac87de6009e40f052cb1a35e3ef1482c8
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2022-11-30 22:02:46 +0300

    configure deployment

--- storefront/settings.py
+++ storefront/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [str(os.getenv("ALLOWED_HOSTS"))]
 
 # Application definition
 INSTALLED_APPS = [
